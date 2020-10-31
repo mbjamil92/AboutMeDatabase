3bc684270125648ece6ac8ae3b1f1b51d0b36297
Muhammad Bilal Jamil <70377341+mbjamil92@users.noreply.github.com>
2020-10-31 09:26:51 -0400

    Add files via upload
    In this SQL script, I've populated the tables with data which will be crucial to display my introduction using the "About Me" database.

---
+++ InsertData.sql
@@ -0,0 +1,42 @@
+INSERT INTO Event_Type 
+VALUES ('A','My Birth’),('B','Move’),('C','School’),('D','University’),('E','First Job’),('F','Fell in Love’),('G','Marriage');
+
+
+INSERT INTO Hero 
+VALUES (1,'Mom&Dad','M’),(2,'Grand-Father','M’),(3,'Mother','F’),(4,'Dad','M’),(5,'Senior','M') (6,'Manal','F’),(7,'Manal','F');
+
+INSERT INTO PERSON
+VALUES 
+(1,'Aroosa & Jamil','Mairaj & Uddin’),(2,'Yasin','Sheikh’),(3,'Aroosa','Mairaj’),(4,'Jamil','Uddin'),(5,'Muhammad','Haseeb’),(6,'Manal','Ahmad’),(7,'Manal','Ahmad,M.D.');
+
+
+INSERT INTO Place
+VALUES
+(1,'Holy Family Hospital','Hospital','Rawalpindi','RWP'),
+(2,'Sheikh House','Childhood Home','Rawalpindi','RWP’),
+(3,'Hamza Army Public School','School','Rawalpindi','RWP'),
+(4,'COMSATS University','Univeristy','Islamabad','ISB’),
+(5,'Higher Education Comission','Government Organization','Islamabad','ISB’),
+(6,'At her Medical College','Our 1st date','Rawalpindi','RWP'),
+(7,'Our houses','We got married','Islamabad','ISB');
+ 
+INSERT INTO Place
+VALUES
+(1,1992,'First Baby of my family, welcome',1,1,'A'),
+(2,1992,'Joint Family Home',2,2,'B’),
+(3,1997,'Best Time of my life',3,3,'C'),
+(4,2012,'Found out a lot about myself',4,4,'D’), 
+(5,2016,'Learned a lot about life',5,5,'E’),
+(6,2012,'She was the one',6,6,'F’),
+(7,2020,'We were together for 8 years',7,7,'G');
+
+INSERT INTO Place
+VALUES 
+('Parents','Best parents ever',1,1,1),
+('Grand Son','Strict and Disciplined',2,2,2),
+('Parent','Worlds best mom',3,3,3),
+('Just like grand-dad','People call me his twin',4,4,4), 
+('Met at a party','Gentle soul',5,5,5),
+('Met at my university','She was giving a presentation & I was organizing that event,
+I talked to her and asked her out',6,6,6),
+('Same old','families got to know each other',7,7,7);
